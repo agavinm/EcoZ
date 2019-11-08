87ec732ec5261e1c8254566f06096bab78a07f04
Andrés <agavinm@gmail.com>
2019-11-08 16:11:31 +0100

    Nuevo proyecto spring

---
+++ Practicas/Practica3/eclipse/ecoz/src/main/java/com/ecoz/repository/EcozApplication.java
@@ -0,0 +1,13 @@
+package com.ecoz.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcozApplication.class, args);
+	}
+
+}
---
+++ Practicas/Practica3/eclipse/ecoz/src/test/java/com/ecoz/repository/EcozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoz.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
